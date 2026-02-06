00a2d97399b93d49398d281a57429905ab97f2a0
Muhammad Zaidan Alhafidz <zaidanalhafidz22@gmail.com>
2026-02-06 17:53:38 +0700

    Create blade.blade.php

---
+++ resources/views/biodata_view/blade.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata</title>
+</head>
+<body>
+    <h1>Biodata Mahasiswa</h1>
+    <hr>
+    <p>Nama: {{ $data['nama'] }}</p>
+    <p>NIM: {{ $data['nim'] }}</p>
+    <p>Prodi: {{ $data['prodi'] }}</p>
+    <p>Kampus: {{ $data['kampus'] }}</p>
+</body>
+</html>
